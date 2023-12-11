9ab973bde72d1baf9010b71fe68b418ebf38eed6
Ryan Pek <ryanpekyize2001@gmail.com>
2023-12-11 13:41:37 +0800

    added 3a solutions

--- CTDterm1butinc/3AbutinC.c
+++ CTDterm1butinc/3AbutinC.c
@@ -63,7 +63,7 @@ float sum_of_series(int n){
     float total=0;
 
     for(int i=1;i<n+1;i++){
-        total+=1.00/((1+i)*(1+i));
+        total+=1.00/((i)*(i));
     }
     return total;
 }
@@ -92,11 +92,59 @@ int is_prime(long long x){
 }
 
 //3.04 number of terms required
+float fraction_of_pisq(float s){
+    return ((6.0*s)/(pow(M_PI,2.0)));
+}
+
+int terms_required(float p){
+    int terms = 0;
+    while (fraction_of_pisq(sum_of_series(terms))<p){
+        terms++;
+    }
+    return terms;
+}
+
+//3.05a calculate_sum_even
+int calculate_sum_even(int n){
+    int total=0;
+    int i=0;
+    while(i<n){
+        total+=i;
+        i+=2;
+    }
+    return total;
+}
+
+//3.05b alternating_while
+float int_abs(float x){
+    if(x<0){
+        return -1*x;
+    }
+    else{
+        return x;
+    }
+}
+
+float alternating_while(float stop){
+    float total=0;
+    int n = 1;
+    float term = 1.0;
+    while(int_abs(term)>stop){
+        total+=term;
+        n++;
+        if(n%2==0){
+            term = -1.0*(1.0/n);
+        }
+        else{
+            term = 1.0/n;
+        }
+    }
+    return total;
+}
 
 
 
-
-int main(int argv, char** argc){
+int main(int argc, char** argv){
 //    //3.00a calculate_sum_odd
 //    printf("%d\n",calculate_sum_odd(0));
 //
@@ -116,7 +164,19 @@ int main(int argv, char** argc){
 //    int pp;
 //    scanf("%d\n",pp);
 //    printf("%d\n",pp);
-    printf("%d\n",is_prime(49));
+    //printf("%d\n",is_prime(49));
+
+//    printf("%f\n", sum_of_series(6));
+//    printf("%f\n", fraction_of_pisq(1.5));
+//    printf("%d\n", terms_required(0.9));
+
+//    printf("%d\n", calculate_sum_even(11));
+//    printf("%d\n", calculate_sum_even(12));
+//    printf("%d\n", calculate_sum_even(13));
+
+    printf("%f\n", alternating_while(0.249));
+    printf("%f\n", alternating_while(0.199));
+
 
     return 0;
 
